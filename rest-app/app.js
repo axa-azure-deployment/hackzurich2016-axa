420baee3c708c1eba9314c3867f6e78dfa265011
Lorenz Hänggi <lolo8304@gmail.com>
2016-08-04 17:50:11 +0200

    update favicon for axa

--- rest-app/app.js
+++ rest-app/app.js
@@ -18,6 +18,7 @@ var axa = require('./routes/axa');
 var users = require('./routes/users');
 
 var app = express();
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(cors({
     allowedOrigins: [
@@ -31,7 +32,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
