2aa8791fdeabcf93dcea5969c25463e83ce7c4ad
Lorenz Hänggi <lolo8304@gmail.com>
2016-08-10 11:31:35 +0200

    document high level intro and count.js

---
+++ rawdata/count.js
@@ -0,0 +1,19 @@
+var importMeta = [
+    { "collection" : "customers", "file" : "./customers/customers.json"},
+    { "collection" : "trips", "file" : "./telematics/all-trips.json"},
+    { "collection" : "transactions", "file" : "./transactions/transactions.json"},
+
+    { "collection" : "trucks", "file" : "./cars/trucks.json"},
+    { "collection" : "cars", "file" : "./cars/cars.json"},
+
+    { "collection" : "insuranceTypes", "file" : "./favorites/insuranceTypes.json"},
+    { "collection" : "categories", "file" : "./favorites/categories.json"},
+    { "collection" : "risks", "file" : "./favorites/risks.json"},
+
+    { "collection" : "favorites", "file" : "./favorites/favorites.json"}
+
+];
+
+for (var i = 0, l = importMeta.length; i < l; i++){
+    print("count of "+importMeta[i].collection+"="+db.getCollection(importMeta[i].collection).count()+ " objects");
+}
