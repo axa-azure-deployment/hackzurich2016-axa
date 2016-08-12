ec2a6fe9c850d747e8bb7f8af01cc4c5f040e70b
Lorenz Hänggi <lolo8304@gmail.com>
2016-08-13 01:07:29 +0200

    import profiles only. to fix fast bugs

---
+++ rawdata/import-profile.js
@@ -0,0 +1,16 @@
+var importMeta = [
+    { "collection" : "profiles", "file" : "./customers/profiles.json"}
+];
+
+for (var i = 0, l = importMeta.length; i < l; i++){
+    print(" start collection "+importMeta[i].collection + " dropping and importing ...");
+    db.getCollection(importMeta[i].collection).drop();
+    db.getCollection(importMeta[i].collection).dropIndex( { "$**": "-1" });
+
+    var fileContent = cat(importMeta[i].file);
+    db.getCollection(importMeta[i].collection).insertMany(JSON.parse(fileContent));
+    db.getCollection(importMeta[i].collection).createIndex( { "$**": "text" });
+    
+    print(" done. imported "+db.getCollection(importMeta[i].collection).count()+ " objects");
+}
+
