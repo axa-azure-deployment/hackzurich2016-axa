f0c690d42fcaab7e2fca18f5da2b40228d0a06a7
Lorenz Hänggi <lolo8304@gmail.com>
2016-08-27 10:11:44 +0200

    update doc with Hololens GI

--- rest-app/dist/copySwagger.sh
+++ rest-app/dist/copySwagger.sh
@@ -8,14 +8,14 @@ do
 	  echo "`date` - copy swagger.json file to git"
 	  mv $LOC/swagger.json $DLOC/swagger.json
 	  echo "`date` - copy swagger.local.json file to git for localhost"
-		sed 's/hackzurich16.azurewebsites.net/localhost\:3000/g' $DLOC/swagger.json > $DLOC/swagger.local.json
+		cat $DLOC/swagger.json | sed 's/hackzurich16.azurewebsites.net/localhost\:3000/g' | sed 's/https:\/\/localhost/http:\/\/localhost/g' > $DLOC/swagger.local.json
 	fi
 	if [ -f "$LOC/swagger.yaml" ]
 	then
 		echo "`date` - copy swagger.yaml file to git"
 		mv $LOC/swagger.yaml $DLOC
 		echo "`date` - copy swagger.local.yaml file to git for localhost"
-		sed 's/hackzurich16.azurewebsites.net/localhost\:3000/g' $DLOC/swagger.yaml > $DLOC/swagger.local.yaml
+		cat $DLOC/swagger.yaml | sed 's/hackzurich16.azurewebsites.net/localhost\:3000/g' | sed 's/https:\/\/localhost/http:\/\/localhost/g' > $DLOC/swagger.local.yaml
 	fi
 	sleep 1
 done
